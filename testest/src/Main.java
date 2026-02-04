8c304a309a5f2646c6f7ffba7b2f3480b9e13cf6
DemiOlushola <118903624+DemiOlushola@users.noreply.github.com>
2026-02-04 18:31:24 +0100

    Delete testest directory

--- testest/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(0.1 * 10);
-        System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1
-                + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
-
-        System.out.println(1 + 22 + "Hello");
-    }
-}
